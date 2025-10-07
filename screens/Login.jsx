1945d63220e6f191d99aeb57b934117b5080c7b4
majuszl <167575638+majuszl@users.noreply.github.com>
2025-10-07 15:02:39 -0300

    login

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -20,7 +20,7 @@ export default function Login({ navigation }) {
     const [successVisible, setSuccessVisible] = useState(false);
     const [errorVisible, setErrorVisible] = useState(false);
     const [messageText, setMessageText] = useState("");
-    const { theme } = useTheme();
+    const {theme} = useTheme();
     const [senhaVisivel, setSenhaVisivel] = useState(false);
 
     const visibilidadeDaSenha = () => {
@@ -39,6 +39,8 @@ export default function Login({ navigation }) {
                     senha: senha.trim()
                 }),
             });
+
+            console.log(retorno);
 
             if (!retorno.ok) {
                 throw new Error("Erro ao se comunicar com o servidor");
@@ -79,6 +81,7 @@ export default function Login({ navigation }) {
             }
 
         } catch (err) {
+            console.log(err)
             setMessageText("Não foi possível conectar ao servidor");
             setErrorVisible(true);
             setSuccessVisible(false);
@@ -88,122 +91,125 @@ export default function Login({ navigation }) {
     }
 
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: theme.background,
-            alignItems: "center",
-            paddingTop: 40,
-        },
-        title: {
-            color: theme.text,
-            fontSize: 26,
-            fontWeight: "bold",
-            marginBottom: 30,
-        },
-        forgotPassword: {
-            color: theme.accent,
-            fontSize: 14,
-            marginVertical: 10,
-            textDecorationLine: "underline",
-        },
-        loadingContainer: {
-            marginTop: 20,
-        },
-        serverInfo: {
-            color: theme.secondaryText,
-            fontSize: 10,
-            textAlign: "center",
-            marginTop: 10,
-            fontStyle: 'italic',
-        },
-        input: {
-            height: 50,
-            borderColor: "#ccc",
-            borderWidth: 1,
-            marginBottom: 15,
-            paddingLeft: 10,
-        },
-        inputContainer: {
-            flexDirection: "row",
-            alignItems: "center",
-            position: "relative",
-        },
-        eyeIconContainer: {
-            position: "absolute",
-            right: 10,
-            padding: 5,
-        },
-        eyeIcon: {
-            width: 27,
-            height: 27,
-            marginTop: 9,
-        },
-    });
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                backgroundColor: theme.background,
+                alignItems: "center",
+                paddingTop: 40,
+            },
+            title: {
+                color: theme.text,
+                fontSize: 26,
+                fontWeight: "bold",
+                marginBottom: 30,
+            },
+            forgotPassword: {
+                color: theme.accent,
+                fontSize: 14,
+                marginVertical: 10,
+                textDecorationLine: "underline",
+            },
+            loadingContainer: {
+                marginTop: 20,
+            },
+            serverInfo: {
+                color: theme.secondaryText,
+                fontSize: 10,
+                textAlign: "center",
+                marginTop: 10,
+                fontStyle: 'italic',
+            },
+            input: {
+                height: 50,
+                borderColor: "#ccc",
+                borderWidth: 1,
+                marginBottom: 15,
+                paddingLeft: 10,
+            },
+            inputContainer: {
+                flexDirection: "row",
+                alignItems: "center",
+                position: "relative",
+            },
+            eyeIconContainer: {
+                position: "absolute",
+                right: 10,
+                padding: 5,
+            },
+            eyeIcon: {
+                width: 27,
+                height: 27,
+                marginTop: 9,
+            },
+        });
 
-    return (
-        <View style={styles.container}>
+        return (
+            <View style={styles.container}>
 
-            <HeaderLogin />
-            <Text style={styles.title}>LOGIN</Text>
+                <HeaderLogin/>
+                <Text style={styles.title}>LOGIN</Text>
 
-            <InputField
-                style={styles.input}
-                label="E-mail"
-                value={email}
-                onChangeText={setEmail}
-                placeholder="Digite seu e-mail"
-                autoCapitalize="none"
-                keyboardType="email-address"
-            />
-            <View style={styles.inputContainer} >
                 <InputField
                     style={styles.input}
-                    label="Senha"
-                    value={senha}
-                    onChangeText={setSenha}
-                    placeholder="Digite sua senha"
-                    secureTextEntry={!senhaVisivel}
+                    label="E-mail"
+                    value={email}
+                    onChangeText={setEmail}
+                    placeholder="Digite seu e-mail"
+                    autoCapitalize="none"
+                    keyboardType="email-address"
+                />
+                <View style={styles.inputContainer}>
+                    <InputField
+                        style={styles.input}
+                        label="Senha"
+                        value={senha}
+                        onChangeText={setSenha}
+                        placeholder="Digite sua senha"
+                        secureTextEntry={!senhaVisivel}
+                    />
+
+                    <TouchableOpacity onPress={visibilidadeDaSenha} style={styles.eyeIconContainer}>
+                        <Image
+                            source={senhaVisivel ? require("../assets/eyeiconview.png") : require("../assets/eyeiconblock.png")}
+                            style={styles.eyeIcon}/>
+                    </TouchableOpacity>
+
+                </View>
+
+                <Pressable
+                    onPress={() => navigation.navigate("ForgotPassword")}
+                    style={{alignSelf: "flex-end", marginRight: 30, marginBottom: 20}}
+                >
+                    <Text style={styles.forgotPassword}>Esqueci minha senha</Text>
+                </Pressable>
+
+                {loading ? (
+                    <ActivityIndicator size="large" color={theme.accent} style={styles.loadingContainer}/>
+                ) : (
+                    <PrimaryButton title="Entrar" onPress={realizarLogin}/>
+                )}
+
+                <Text style={styles.serverInfo}>
+                    Conectando em: {"http://10.92.3.155:5000"}
+                </Text>
+
+                <FooterLogo/>
+                <SuccessMessage
+                    visible={successVisible}
+                    message={messageText}
+                    onClose={() => {
+                        setSuccessVisible(false);
+                        navigation.replace("PaginaInicial");
+                    }}
+                />
+                <ErrorMessage
+                    visible={errorVisible}
+                    message={messageText}
+                    onClose={() => setErrorVisible(false)}
                 />
 
-                <TouchableOpacity onPress={visibilidadeDaSenha} style={styles.eyeIconContainer}>
-                    <Image source={senhaVisivel ? require("../assets/eyeiconview.png") : require("../assets/eyeiconblock.png")} style={styles.eyeIcon}/>
-                </TouchableOpacity>
-
             </View>
-
-            <Pressable
-                onPress={() => navigation.navigate("ForgotPassword")}
-                style={{ alignSelf: "flex-end", marginRight: 30, marginBottom: 20 }}
-            >
-                <Text style={styles.forgotPassword}>Esqueci minha senha</Text>
-            </Pressable>
-
-            {loading ? (
-                <ActivityIndicator size="large" color={theme.accent} style={styles.loadingContainer} />
-            ) : (
-                <PrimaryButton title="Entrar" onPress={realizarLogin} />
-            )}
-
-            <Text style={styles.serverInfo}>
-                Conectando em: {"http://127.0.0.1:5000"}
-            </Text>
-
-            <FooterLogo />
-            <SuccessMessage
-                visible={successVisible}
-                message={messageText}
-                onClose={() => {
-                    setSuccessVisible(false);
-                    navigation.replace("PaginaInicial");
-                }}
-            />
-            <ErrorMessage
-                visible={errorVisible}
-                message={messageText}
-                onClose={() => setErrorVisible(false)}
-            />
-
-        </View>
-    );
+        );
+    }
 }
